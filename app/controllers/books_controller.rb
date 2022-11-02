2b12ca49c0525d0b60743b58155f38a6b5755ca3
EC2 Default User <ec2-user@ip-172-31-0-61.ap-northeast-1.compute.internal>
2022-11-02 17:17:42 +0000

    [checksheet-20]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,39 @@
 class BooksController < ApplicationController
   def new
+    @book = Book.new
   end
 
   def index
+    @books = Book.all
   end
 
   def show
+    @book = Book.find(params[:id])  
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+  
+  def create
+    # １.&2. データを受け取り新規登録するためのインスタンス作成
+    book = Book.new(book_params)
+    
+    # 3. データをデータベースに保存するためのsaveメソッド実行
+    book.save
+    
+    redirect_to book_path(book.id)
+  end  
+    
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)  
+  end
+  
+   private
+  # ストロングパラメータ
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   root to: 'homes#top'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :books
+  get 'books/new'
+  post 'books'=> 'books#create'
+  get 'books/:id' => 'books#show',as: 'book'
+  get 'books/:id/edit' => 'books#edit', as: 'edit_book'
+  get 'books' => 'books#index'
+  patch 'books/:id' => 'books#update', as: 'update_book'
 end
