42d208a169e9655212b83ba524544f02c4fd5050
EC2 Default User <ec2-user@ip-172-31-0-61.ap-northeast-1.compute.internal>
2022-11-08 11:01:11 +0000

    [Bookers-finish]

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   root to: 'homes#top'
   resources :books
+  #post 'books'=> 'books#create'
+  #get 'books/:id' => 'books#show',as: 'book'
+  #get 'books/:id/edit' => 'books#edit', as: 'edit_book'
+  #get 'books' => 'books#index'
+  #patch 'books/:id' => 'books#update', as: 'update_book'
+  #delete 'books/:id' => 'books#destroy', as: 'destroy_book'
 end
